04f96ccb8c11e52cc6e21c9552b281ad5ccefc32
jaubert-nicolas <nicolas.jaubert@etu.mines-ales.fr>
2025-02-24 11:48:17 +0100

    message 2

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
+resource "docker_image" "nginx" {
+  name = var.image
+}
+
 resource "docker_container" "nginx" {
   count = var.num_containers
 
-  image = var.image
+  image = docker_image.nginx.latest
   name  = "nginx_container_${count.index}"
 
   memory = var.memory
@@ -10,6 +14,16 @@ resource "docker_container" "nginx" {
   ports {
     internal = 80
     external = var.starting_port + count.index
+  }
+
+  upload {
+    content = file("${path.module}/nginx.conf")
+    file    = "/etc/nginx/conf.d/default.conf"
+  }
+
+  upload {
+    content = file("${path.module}/index.html")
+    file    = "/usr/share/nginx/html/index.html"
   }
 }
 
